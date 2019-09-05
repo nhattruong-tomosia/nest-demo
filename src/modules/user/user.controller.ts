6f691bbf7dbc5636167de6340e697a721c68461b
Minh Nhat <nhat.truong@tomosia.com>
2019-09-05 11:05:55 +0700

    add result transform interceoptor

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,17 +1,27 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
 import { UserService } from './user.service';
+import { LoggingInterceptor } from '../../shared/interceptor/logger.interceptor';
+import { ErrorInterceptor } from '../../shared/interceptor/errors.interceptor';
+import { WsException } from '@nestjs/websockets';
+import { ResultTransform } from '../../shared/interceptor/resultTransform.interceptor';
 
-@Controller('user')
+@UseInterceptors(LoggingInterceptor)
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
+
   @Get()
+  @UseInterceptors(ResultTransform)
   getAllUser() {
     return this.userService.findAll()
   }
 
-  @Get('id')
+
+  @Get(':id')
+  // @UseInterceptors(ErrorInterceptor)
   getUserById(@Param() id) {
+    // throw new WsException('Missing entry id.');
     return this.userService.findById(id)
   }
 }
--- src/shared/interceptor/resultTransform.interceptor.ts
+++ src/shared/interceptor/resultTransform.interceptor.ts
@@ -1,10 +1,13 @@
 import { Injectable, NestInterceptor, CallHandler, ExecutionContext } from '@nestjs/common'
 import { Observable } from 'rxjs'
-
+import { map } from 'rxjs/operators'
 @Injectable()
 export class ResultTransform implements NestInterceptor {
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle()
+      .pipe(map(data => {
+        return { data, count: data.length }
+      }))
   }
 }
